eef4ab2c7556d0d4893920001cba098a08cbe982
rpurdon <ross.leonard.pur@gmail.com>
2016-01-30 19:06:45 +0200

    fix errors

--- TankController/switch.py
+++ TankController/switch.py
@@ -1,13 +1,11 @@
 import RPi.GPIO as gpio
 
-GPIO.setmode(GPIO.BOARD)
-
 class switch(object):
   def __init__(self, vccpin=0, ch1=0, ch2=0):
     self.vccpin = vccpin
     self.ch1 = ch1
     self.ch2 = ch2
-
+    gpio.setmode(GPIO.BOARD)
     self.gpioSetup
 
   def gpioSetup(self):
@@ -23,12 +21,12 @@ class switch(object):
 
   def on(self):
     gpio.output(self.ch1, 1)
-    gpio.output(self.ch1, 1)
+    gpio.output(self.ch2, 1)
 
   def off(self):
     gpio.output(self.ch1, 0)
-    gpio.output(self.ch1, 0)
+    gpio.output(self.ch2, 0)
 
   def idle(self):
     gpio.output(self.ch1, 0)
-    gpio.output(self.ch1, 1)
+    gpio.output(self.ch2, 1)
